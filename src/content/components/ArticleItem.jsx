a16b9fa0b7f93b1a59e0fa4c2c7b367f178bd08a
Shivam Gupta <shivamg8797@gmail.com>
2024-05-22 13:15:16 +0530

    responsive queries added

--- src/content/components/ArticleItem.jsx
+++ src/content/components/ArticleItem.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const ArticleItem = ({ article, link }) => {
+const ArticleItem = ({ article }) => {
+  const [imageError, setImageError] = useState(false);
+
+  const handleImageError = () => {
+    setImageError(true);
+  };
+
+  const handleImageLoad = () => {
+    setImageError(false);
+  };
+
   return (
-    <div className={"card card-news"}>
-      <img
-        src={article?.urlToImage || "https://picsum.photos/200/300"}
-        alt={article.title}
-        className="article-image"
-      />
-      <h2>{article.title}</h2>
-      <p>{article.description}</p>
-      {/* <p>Article By:{article.author}</p> */}
-      {/* <Link to={`/article/${article.id}}`}>Read more</Link> */}
+    <div className="card card-news">
+      <Link to={`/article/${article.id}`}>
+        {(!imageError && (
+          <img
+            src={article?.urlToImage || "https://picsum.photos/200/300"}
+            alt="Unable to load article image"
+            className="article-image"
+            onError={handleImageError}
+            onLoad={handleImageLoad}
+          />
+        )) || (
+          <img
+            src={"https://picsum.photos/200/300"}
+            alt="Unable to load article image"
+            className="article-image"
+          />
+        )}
+        <h2>{article.title}</h2>
+        <p>{article.description}</p>
+        {/* <p>Article By:{article.author}</p> */}
+      </Link>
     </div>
   );
 };
--- src/content/components/Menu.jsx
+++ src/content/components/Menu.jsx
@@ -14,6 +14,7 @@ const Menu = () => {
   const dispatch = useDispatch();
 
   const handleSettingsClick = () => {
+    localStorage.removeItem("preferences");
     dispatch(setShowPreferencePopup(true));
   };
   const handleMenuClick = () => {
